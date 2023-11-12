6caeb85a3d78afd4c5cd82b927a01bb287fc8dbb
Jonathan <Jonathan.Hwang.93@gmail.com>
2023-11-11 23:30:13 -0600

    work

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,4 +1,4 @@
 export async function load({ fetch }) {
-	const contributions = await (await fetch('mattcroat/2021')).json()
+	const contributions = await (await fetch('CCKJon/2023')).json()
 	return { contributions }
 }
